9fb1e043580693030c794459615d88fb2e2ba367
KhanhLy <20070745@vnu.edu.vn>
2023-04-09 16:06:14 +0700

    Add model validation and association

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :subject
+  validates_presence_of :title, :author, :publisher
+  validates_uniqueness_of :title
+  validates_numericality_of :year
+  belongs_to :subject
 end
